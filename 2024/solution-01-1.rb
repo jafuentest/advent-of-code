1ace9375be46e7ff32a504826d38234767fb6c63
Juan Fuentes <jn.fuentest@gmail.com>
2024-12-01 17:57:21 +0100

    Day one

---
+++ 2024/solution-01-1.rb
@@ -0,0 +1,18 @@
+list1 = []
+list2 = []
+diff = []
+
+File.foreach('input-01.txt') do |line|
+  list_items = line.split
+  list1 << list_items.first.to_i
+  list2 << list_items.last.to_i
+end
+
+list1.sort!
+list2.sort!
+
+list1.each_with_index do |item1, i|
+  diff << (item1 - list2[i]).abs
+end
+
+puts diff.sum
