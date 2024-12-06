a64e1c7f94ac1c4159997750cba725189425d5fa
Juan Fuentes <jn.fuentest@gmail.com>
2024-12-06 02:55:33 +0100

    Day two

---
+++ 2024/solution-02-1.rb
@@ -0,0 +1,18 @@
+def safe?(levels)
+  ascending = levels[0] > levels[1]
+  levels[1..].each_with_index do |level, i|
+    diff = levels[i] - level
+
+    return false if diff == 0 || diff.abs > 3 || (diff.positive? ^ ascending)
+  end
+
+  true
+end
+
+safe_count = 0
+
+File.foreach('input-02.txt') do |line|
+  safe_count += 1 if safe?(line.split.map(&:to_i))
+end
+
+puts safe_count
---
+++ 2024/solution-02-2.rb
@@ -0,0 +1,29 @@
+def safe?(levels)
+  ascending = levels[0] > levels[1]
+  levels[1..].each_with_index do |level, i|
+    diff = levels[i] - level
+
+    return false if diff == 0 || diff.abs > 3 || (diff.positive? ^ ascending)
+  end
+
+  true
+end
+
+def soft_safe?(levels)
+  return true if safe?(levels)
+
+  (0...levels.size).any? do |i|
+    modified_array = levels.dup
+    modified_array.delete_at(i)
+    safe?(modified_array)
+  end
+end
+
+safe_count = 0
+
+File.foreach('input-02.txt') do |line|
+  levels = line.split.map(&:to_i)
+  safe_count += 1 if soft_safe?(levels)
+end
+
+puts safe_count
