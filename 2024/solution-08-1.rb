331bef625a8864415b15025c1efdb9872438ceb1
Juan Fuentes <jn.fuentest@gmail.com>
2024-12-08 16:46:46 +0100

    Day 6

---
+++ 2024/solution-08-1.rb
@@ -0,0 +1,25 @@
+max = -1
+data = Hash.new { |h, k| h[k] = [] }
+File.foreach('input-08.txt') do |line|
+  max += 1
+  line.chars.each_with_index do |char, j|
+    data[char] << [max, j] unless char == '.'
+  end
+end
+
+points = Set.new
+data.each_value do |coords|
+  coords.combination(2).each do |a, b|
+    dx = b[0] - a[0]
+    dy = b[1] - a[1]
+
+    c1 = [a[0] - dx, a[1] - dy]
+    c2 = [b[0] + dx, b[1] + dy]
+
+    [c1, c2].each do |c|
+      points.add(c) if c.all? { |e| e >= 0 && e <= max }
+    end
+  end
+end
+
+puts points.size
