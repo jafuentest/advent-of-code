079ecba848b6fa491cac7a28c072b5a99c3f4e6a
Juan Fuentes <jn.fuentest@gmail.com>
2024-12-13 19:46:19 +0100

    Day 11

---
+++ 2024/solution-13-1.rb
@@ -0,0 +1,22 @@
+TOLERANCE = 1e-3
+def integer?(number)
+  (number - number.round).abs < TOLERANCE
+end
+
+games = File.read('input-13.txt').split("\n\n")
+
+costs = games.map do |game|
+  btn_a, btn_b, target = game.split("\n")
+    .map { |e| e.scan(/\d+/).map(&:to_f) }
+
+  ax, ay = btn_a
+  bx, by = btn_b
+  x, y = target
+
+  a = (x - (y * bx / by)) / (ax - (ay * bx / by))
+  b = (y - (a * ay)) / by
+
+  integer?(a) && integer?(b) ? (a * 3) + b : 0
+end
+
+puts costs.sum
