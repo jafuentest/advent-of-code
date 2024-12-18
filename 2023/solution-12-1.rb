d960a90358ded70eb5b077ab8ab91c828850affb
Juan Fuentes <jn.fuentest@gmail.com>
2024-12-18 02:23:13 +0100

    2023 Day 12

---
+++ 2023/solution-12-1.rb
@@ -0,0 +1,21 @@
+$variants = {}
+
+def find_variants(str)
+  return $variants[str] if $variants.key?(str)
+
+  return $variants[str] = [str] unless str.include? '?'
+
+  variants = find_variants(str.sub('?', '.')) + find_variants(str.sub('?', '#'))
+  $variants[str] = variants
+end
+
+total = File.read('input-12.txt').lines.reduce(0) do |t, line|
+  string, numbers = line.split
+  numbers = numbers.split(',').map(&:to_i)
+
+  t + find_variants(string).count do |variant|
+    numbers == variant.split('.').reject(&:empty?).map(&:size)
+  end
+end
+
+puts total
---
+++ 2023/solution-12-2.rb
@@ -0,0 +1,73 @@
+result = 0
+
+File.foreach('input-12.txt') do |line|
+  first, second = line.split
+  prefix = first.chars
+  suffix = second.split(',').map(&:to_i)
+
+  pattern = []
+  springs = []
+
+  # Unfold the records
+  4.times do
+    pattern += prefix
+    pattern << '?'
+    springs += suffix
+  end
+
+  # Add trailing '.' so we don't check bounds for the last pattern.
+  pattern += prefix
+  pattern << '.'
+  springs += suffix
+
+  # Calculate prefix sum of broken springs and unknowns before each index.
+  sum = 0
+  broken = [0]
+  pattern.each_with_index do |b, i|
+    sum += 1 if b != '.'
+    broken[i + 1] = sum
+  end
+
+  # Count combinations, handling first row as a special case.
+  table = []
+  first_size = springs[0]
+  sum = 0
+  valid = true
+  wiggle = pattern.size - springs.sum - springs.size + 1
+  wiggle.times do |i|
+    if pattern[i + first_size] == '#'
+      sum = 0
+    elsif valid && broken[i + first_size] - broken[i] == first_size
+      sum += 1
+    end
+
+    table[i + first_size] = sum
+    valid &&= pattern[i] != '#'
+  end
+
+  # Count each subsequent spring.
+  start = first_size + 1
+  springs[1..].each_with_index do |size, row|
+    previous = row * pattern.size
+    current = (row + 1) * pattern.size
+    sum = 0
+
+    (start...(start + wiggle)).each do |i|
+      if pattern[i + size] == '#'
+        sum = 0
+      elsif table[previous + i - 1] > 0 &&
+            pattern[i - 1] != '#' &&
+            broken[i + size] - broken[i] == size
+        sum += table[previous + i - 1]
+      end
+
+      table[current + i + size] = sum
+    end
+
+    start += size + 1
+  end
+
+  result += sum
+end
+
+puts result
