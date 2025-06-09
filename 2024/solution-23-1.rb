f14319cd646bfe714cd0a875517de06416d7c01e
Juan Fuentes <jn.fuentest@gmail.com>
2025-06-10 00:05:56 +0200

    Day 20

---
+++ 2024/solution-23-1.rb
@@ -0,0 +1,30 @@
+PC_ID_MAP = ('aa'..'zz').map.with_index { |c, i| [c, i] }.to_h
+POSSIBLE_IDS = PC_ID_MAP.select { |pc, _id| pc.start_with?('t') }.values.to_set
+
+pcs = Set.new
+n = PC_ID_MAP.size
+adj_matrix = Array.new(n) { Array.new(n, false) }
+
+File.foreach('input-23.txt', chomp: true) do |line|
+  pc1, pc2 = line.split('-').map { |pc| PC_ID_MAP[pc] }
+  pcs.merge([pc1, pc2])
+  adj_matrix[pc1][pc2] = true
+  adj_matrix[pc2][pc1] = true
+end
+
+triangles = []
+(0...n).each do |i|
+  next unless pcs.include?(i)
+
+  (i + 1...n).each do |j|
+    next unless pcs.include?(j)
+
+    (j + 1...n).each do |k|
+      if adj_matrix[i][j] && adj_matrix[j][k] && adj_matrix[k][i]
+        triangles << [i, j, k] if [i, j, k].any? { |id| POSSIBLE_IDS.include?(id) }
+      end
+    end
+  end
+end
+
+puts triangles.size
